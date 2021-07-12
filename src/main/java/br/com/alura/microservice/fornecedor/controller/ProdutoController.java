ea82f1c053c17876ef0251157b5c6312a3f20575
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-07-12 07:48:56 -0300

    adicionando arquivos

--- src/main/java/br/com/alura/microservice/fornecedor/controller/ProdutoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/ProdutoController.java
@@ -1,23 +1,25 @@
 package br.com.alura.microservice.fornecedor.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
-import br.com.alura.microservice.fornecedor.service.InfoService;
+import br.com.alura.microservice.fornecedor.model.Produto;
+import br.com.alura.microservice.fornecedor.service.ProdutoService;
 
 @RestController
-@RequestMapping("/info")
-public class InfoController {
+@RequestMapping("/produto")
+public class ProdutoController {
 
 	@Autowired
-	private InfoService infoService;
+	private ProdutoService produtoService;
 	
 	@GetMapping("/{estado}")
-	public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
-		return infoService.getInfoPorEstado(estado);
+	public List<Produto> getProdutosPorEstado(@PathVariable("estado") String estado) {
+		return produtoService.getProdutosPorEstado(estado);
 	}
 }
