464e56ad8375b47efd5ae172c8becb72a9a874d7
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-07-14 08:03:58 -0300

    adicionando LOG e implementando integracao com Papertrail

--- src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.fornecedor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,14 @@ import br.com.alura.microservice.fornecedor.service.InfoService;
 @RequestMapping("/info")
 public class InfoController {
 
+	private static Logger LOG = LoggerFactory.getLogger(InfoController.class);
+	
 	@Autowired
 	private InfoService infoService;
 	
 	@GetMapping("/{estado}")
 	public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
+		LOG.info("recebido pedido de informacoes do fornecedor de {}",estado);
 		return infoService.getInfoPorEstado(estado);
 	}
 }
--- src/main/java/br/com/alura/microservice/fornecedor/controller/PedidoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/PedidoController.java
@@ -2,13 +2,14 @@ package br.com.alura.microservice.fornecedor.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.microservice.fornecedor.controller.dto.ItemDoPedidoDTO;
@@ -19,11 +20,14 @@ import br.com.alura.microservice.fornecedor.service.PedidoService;
 @RequestMapping("pedido")
 public class PedidoController {
 
+	private static Logger LOG = LoggerFactory.getLogger(PedidoController.class);
+
 	@Autowired
 	private PedidoService pedidoService;
 	
 	@PostMapping
 	public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
+		LOG.info("Pedido recebido");
 		return pedidoService.realizaPedido(produtos);
 	}
 	
